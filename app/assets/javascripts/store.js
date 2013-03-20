0ceba33b0ec34d13dc6b7927a6348460e9d88889
benburton <ben@neo.com>
2013-03-19 20:53:29 -0400

    Much more concise attaching of tokens to requests

--- app/assets/javascripts/store.js
+++ app/assets/javascripts/store.js
@@ -5,7 +5,7 @@ $(document).ajaxError(function(event, jqXHR, ajaxSettings, thrownError) {
 });
 
 
-Trakio.Adapter = DS.RESTAdapter.extend()
+Trakio.Adapter = DS.RESTAdapter.extend();
 
 Trakio.Adapter.configure("plurals", { story: 'stories' });
 
@@ -13,30 +13,10 @@ Trakio.Store = DS.Store.extend({
   revision: 11,
   adapter: Trakio.Adapter.extend({
     bulkCommit: false,
-
-    // TODO: Remove me when Ember gets sane token management
-    oldAjax: function(url, type, hash) {
-      hash.url = url;
-      hash.type = type;
-      hash.dataType = 'json';
-      hash.contentType = 'application/json; charset=utf-8';
-      hash.context = this;
-
-      if (hash.data && type !== 'GET') {
-        hash.data = JSON.stringify(hash.data);
-      }
-
-      jQuery.ajax(hash);
-    },
-
-    ajax: function(url, type, hash) {
+    buildURL: function(record, suffix) {
+      var baseUrl = this._super(record, suffix);
       var token = Trakio.get('authentication_token');
-      if (token) {
-        hash.data || (hash.data = {});
-        hash.data['auth'] = token;
-      }
-      hash.context = this;
-      return this.oldAjax(url, type, hash);
+      return token ? baseUrl + '?auth=' + token : baseUrl;
     }
   })
 });
