c2f19bc9071b76dcd78a7ab123ecbb26887bc497
benburton <ben@neo.com>
2013-03-28 13:34:32 -0400

    Logout doesn't produce unnecessary 401.

--- app/assets/javascripts/controllers/logout_controller.js
+++ app/assets/javascripts/controllers/logout_controller.js
@@ -2,6 +2,6 @@ Trakio.LogoutController = Ember.Controller.extend({
   logout: function() {
     $.cookie('authentication_token', undefined);
     Trakio.set('authentication_token', undefined);
-    this.transitionToRoute('index');
+    this.transitionToRoute('login');
   }
 });
