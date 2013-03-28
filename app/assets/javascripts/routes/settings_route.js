8baaf4c1b4b907c3842b10c8db7e5f7d80420a80
benburton <ben@neo.com>
2013-03-28 11:27:51 -0400

    Remove unnecessary controller setup

--- app/assets/javascripts/routes/settings_route.js
+++ app/assets/javascripts/routes/settings_route.js
@@ -1,8 +1,5 @@
 Trakio.SettingsRoute = Ember.Route.extend({
   model: function() {
     return Trakio.User.find('me');
-  },
-  setupController: function(controller, model) {
-    controller.set('user', model);
   }
 });
